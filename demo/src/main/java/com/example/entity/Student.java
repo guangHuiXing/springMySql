49747aa87ff1c2948d0e6c0d264e7d9f9807d7e3
xinggh <1qaz!QAZ>
2019-05-16 11:12:30 +0800

    mybatis 添加缓存

--- demo/src/main/java/com/example/entity/Student.java
+++ demo/src/main/java/com/example/entity/Student.java
@@ -2,11 +2,13 @@ package com.example.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by XGH on 2019/5/15
  */
 @Data
-public class Student {
+public class Student implements Serializable {
     private String name;
     private int id;
     private int teacherId;
